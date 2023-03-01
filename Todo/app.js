d8b23a8b12977fa436d9aed95efa1b3e4e8be2a8
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2023-03-01 20:35:27 +0800

    hh

--- Todo/app.js
+++ Todo/app.js
@@ -2,7 +2,20 @@ const TodoIn = document.querySelector('#todoIn');
 const add = document.querySelector('#addBtn');
 const listTodo = document.querySelector(".list-todo");
 
-addEventListener('load',show)
+addEventListener('load',ShowList);
+
+ const todo = class{
+   constructor(id,todolist){
+      this.id = id,
+      this.check = false;
+      this.todolist = todolist
+   }
+ }
+
+ function randomId(){
+   let id = Math.random().toString().split('.')[1];
+   return id
+ }
 
 add.addEventListener('click',createrTodo)
 
@@ -11,61 +24,73 @@ function createrTodo(){
     let Todotext = document.createElement('input');
     let DltBtn = document.createElement('button');
     let check =document.createElement('button');
-    check.innerHTML = `<i class="fa-solid fa-check"></i>`
+    let todoObject = new todo(randomId(),TodoIn.value)
+
+    check.innerHTML = `<i class="fa-solid fa-check"></i>`;
     DltBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-    Todotext.setAttribute('readonly','readonly')
-    Todotext.value = TodoIn.value;
+    DltBtn.setAttribute('data-id',`${todoObject.id}`);
+   check.setAttribute('data-id',`${todoObject.id}`);
+
+    Todotext.setAttribute('readonly','readonly');
+    Todotext.value = todoObject.todolist;
     TodoList.appendChild(Todotext);
     TodoList.appendChild(check);
     TodoList.appendChild(DltBtn);
     listTodo.appendChild(TodoList);
-    savelocal(TodoIn.value);
 
+   SaveLocal(todoObject);
 }
+let arr  =[]
 
-let arr = []
-
-function savelocal(arg){
-   if(localStorage.getItem('todo')==null){
+function SaveLocal(arg){
+   if(localStorage.getItem('todo')===null){
       arr.push(arg)
       localStorage.setItem('todo',JSON.stringify(arr));
    }else{
-      let localarr =JSON.parse( localStorage.getItem('todo'));
+      let localarr = JSON.parse(localStorage.getItem('todo'));
       localarr.push(arg);
-      localStorage.setItem('todo',JSON.stringify(localarr))
-     
+      localStorage.setItem('todo',JSON.stringify(localarr));
+      
    }
    
-
 }
 
-
-function show(){
-   let showArr = JSON.parse(localStorage.getItem('todo'))
-   showArr.forEach(element => {
-      let TodoList = document.createElement('li');
-      let Todotext = document.createElement('input');
-      let DltBtn = document.createElement('button');
-      let check =document.createElement('button');
-      check.innerHTML = `<i class="fa-solid fa-check"></i>`
-      DltBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-      Todotext.setAttribute('readonly','readonly')
-      Todotext.value = element;
-      TodoList.appendChild(Todotext);
-      TodoList.appendChild(check);
-      TodoList.appendChild(DltBtn);
-      listTodo.appendChild(TodoList);
-      
-   });
+function ShowList(){
+  let x = JSON.parse(localStorage.getItem('todo'))
+  x.forEach(element => {
+   let TodoList = document.createElement('li');
+   let Todotext = document.createElement('input');
+   let DltBtn = document.createElement('button');
+   let check =document.createElement('button');
+   check.innerHTML = `<i class="fa-solid fa-check"></i>`;
+   DltBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+   DltBtn.setAttribute('data-id',`${element.id}`);
+   check.setAttribute('data-id',`${element.id}`);
+   Todotext.setAttribute('readonly','readonly');
+   if(element.check == true){
+      Todotext.classList.add('delete');
+   }
+   Todotext.value = element.todolist;
+   TodoList.appendChild(Todotext);
+   TodoList.appendChild(check);
+   TodoList.appendChild(DltBtn);
+   listTodo.appendChild(TodoList);
+  });
+   
 }
-
 listTodo.addEventListener('click',Dlt);
 
 function Dlt(e){
    let targetEl = e.target;
    if( targetEl.className == "fa-solid fa-trash"){
     let parentEl = targetEl.parentElement;
-    let deleteEl = parentEl.parentElement
+    let deleteEl = parentEl.parentElement;
+    let id = parentEl.dataset.id;
+    let deleteData = JSON.parse(localStorage.getItem('todo'));
+    let deleteArr = deleteData.filter((el)=>{
+      return el.id !== id
+    });
+    localStorage.setItem('todo',JSON.stringify(deleteArr))
     deleteEl.remove()
    }
 }
@@ -78,6 +103,17 @@ function Check(e){
     let parentEl = targetEl.parentElement;
     let elementP = parentEl.parentElement;
     let check = elementP.children[0];
+    let id = parentEl.dataset.id;
+    let checkData = JSON.parse(localStorage.getItem('todo'));
+    let checked = checkData.map((el)=>{
+      if(el.id==id){
+         el.check=true;
+
+      }
+      return el;
+    });
+   console.log(checked);
+   localStorage.setItem('todo',JSON.stringify(checked));
     check.classList.toggle('delete');
    }
 }
