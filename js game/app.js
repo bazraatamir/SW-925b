09130b98ea275373a78aed7ddc8d4394c3376d6a
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2023-03-13 19:25:32 +0800

    js game

--- js game/app.js
+++ js game/app.js
@@ -1,26 +1,72 @@
 let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
-canvas.width = innerWidth>innerHeight?innerHeight:innerWidth;
-canvas.heigth = innerWidth>innerHeight?innerHeight:innerWidth;
-addEventListener('resize',()=>{
-    canvas.width = innerWidth>innerHeight?innerHeight:innerWidth;
-    canvas.heigth = innerWidth>innerHeight?innerHeight:innerWidth;
-})
+const TEIL_COUNT = 17;
+const tali_size = canvas.width / TEIL_COUNT;
+let x = Math.floor(TEIL_COUNT/2);
+let y = Math.floor(TEIL_COUNT/2);
+let vx = 0;
+let vy = 0;
 
-function drawingShapes(){
-    ctx.fillStyle="red"
-    ctx.strokeStyle='red';
+const move = (e)=>{
+    switch (e.key){
+        case 'w':
+            vy=-1
+            vx=0;
+            break;
+        case "s":
+            vy=1;
+            vx=0
+            break
+        case "a":
+            vx=-1;
+            vy =0;
+            break
+        case "d":
+            vx=1;
+            vy=0
+            break
+    }
+        
+ }
 
+addEventListener('keydown',move);
+
+
+
+Animeted()
+function Animeted(){
+   drawingFeild();
+   drawingSnake();
+
+   x+=vx;
+   y+=vy;
+   setTimeout(Animeted,1000/9)
+   
+}
+function drawingFeild(){
+    for(var i = 0; i<TEIL_COUNT; i++){
+        for(var j = 0; j<TEIL_COUNT;j++){
+            if((i+j)%2==0){
+                ctx.fillStyle="#A2D149";
+            }else{
+                ctx.fillStyle="#AAD751"
+            }
+        
+            ctx.beginPath();
+            ctx.fillRect(i*tali_size,j*tali_size,tali_size,tali_size);
+            ctx.closePath();
+        }
+       
+
+}
+}
+
+
+function drawingSnake(){
     ctx.beginPath();
-    ctx.moveTo(20,20);
-    ctx.lineTo(20,100);
-    ctx.lineTo(70,100);
-    ctx.lineTo(70,20);
-    ctx.lineTo(20,20)
+    ctx.fillStyle="red"
+    ctx.arc(x*tali_size-tali_size/2,y*tali_size-tali_size/2,tali_size/2,0,Math.PI*2)
     ctx.stroke();
     ctx.fill()
     ctx.closePath();
-   
 }
-
-drawingShapes()
